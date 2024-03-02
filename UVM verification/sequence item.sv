80e176955184023803fc03fb6e93687bbda925ba
Vyom <97853828+vyom-elan@users.noreply.github.com>
2024-03-02 20:18:08 +0530

    Update sequence item.sv

--- UVM verification/sequence item.sv
+++ UVM verification/sequence item.sv
@@ -1,23 +1,18 @@
-class data_transaction extends uvm_sequence_item;
-	rand logic [15:0] data_in;
-	rand logic put;
-	rand logic get;
-	function new(string name="data_transaction");
-		super.new(name);
-	endfunction
-	`uvm_object_utils_begin(data_transaction)
-	`uvm_field_int(data_in,UVM_ALL_ON)
-	`uvm_field_int(put,UVM_ALL_ON)
-	`uvm_field_int(get,UVM_ALL_ON)
-	`uvm_object_utils_end
-endclass
+// TRANSACTION CLASS
+class fifo_transaction extends uvm_sequence_item;
 
-class rst_transaction extends uvm_sequence_item;
-	logic rst;
-	function new(string name="rst_transaction");
-		super.new(name);
-	endfunction
-	`uvm_object_utils_begin(rst_transaction)
-	`uvm_field_int(rst,UVM_ALL_ON)
-	`uvm_object_utils_end
+    rand logic [7:0] data_in;                                            // Randomized input data
+    rand logic rd_en;                                                    // Read enable flag
+    rand logic wr_en;                                                    // Write enable flag
+  
+    function new(string name = "fifo_transaction");                      // Constructor
+        super.new(name);
+    endfunction
+
+    `uvm_object_utils_begin(fifo_transaction)                            // Function to display transaction information
+        `uvm_field_int(data_in, UVM_ALL_ON)
+        `uvm_field_int(rd_en, UVM_ALL_ON)
+        `uvm_field_int(wr_en, UVM_ALL_ON)
+    `uvm_object_utils_end
+
 endclass
