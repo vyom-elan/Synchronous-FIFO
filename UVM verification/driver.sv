8a4326100b3829fcf136302931947535e8c47832
Vyom <97853828+vyom-elan@users.noreply.github.com>
2024-03-03 22:12:15 +0530

    Update driver.sv

--- UVM verification/driver.sv
+++ UVM verification/driver.sv
@@ -1,64 +1,59 @@
-class data_driver extends uvm_driver#(data_transaction);
-	`uvm_object_utils(data_driver)
-	virtual dut_if driver2dut;
-	data_transaction tr;
-	function new(string name, uvm_component parent);
-		super.new(name, parent);
-	endfunction
-	function void build_phase(uvm_phase phase);
-		super.build_phase(phase);
-		if(!uvm_config_db#(virtual dut_if)::get(this,"","dut_if",driver2dut))
-			`uvm_info("DATA_DRIVER", "uvm_config_db::get failed!", UVM_HIGH)
-	endfunction
-	task reset_check();
-		forever begin
-		@(posedge driver2dut.clk);
-			if(driver2dut.reset)begin
-				driver2dut.put=1'b0;
-				driver2dut.get=1'b0;
-			end
-		end
-	endtask
-		task send_data();
-			forever begin
-			@(posedge driver2dut.clk);
-				if(!driver2dut.reset)begin
-					seq.item_port.get_next_item(tr);
-					#1
-					driver2dut.put=tr.put;
-					driver2dut.get=tr.get;
-					driver2dut.data_in=tr.data_in;
-					seq_item_port.item_done();
-				end
-			end
-		endtask
-		task run_phase(uvm_phase phase);
-			fork	
-				reset_check();
-				send_data();
-			join
-		endtask
-endclass:data_driver
+// DRIVER
+class fifo_driver extends uvm_driver#(fifo_transaction);
 
-class rst_driver extends uvm_driver#(rst_transaction);
-	`uvm_object_utils(rst_driver)
-	rst_transaction tr;
-	virtual dut_if driver2dut;
-	function new(string name, uvm_component parent);
-		super.new(name, parent);
-	endfunction
-	function void build_phase(uvm_phase phase);
-		if(!uvm_config_db#(virtual dut_if)::get(this,"","dut_if",driver2dut))
-			`uvm_info("RST_DRIVER", "uvm_config_db::get failed!", UVM_HIGH)
-	endfunction
-	task run_phase(uvm_phase phase);
-		seq_item_port.get_next_item(tr);
-		driver2dut.reset=tr.rst;
-		repeat(5)@(negedge driver2dut.clk);
-		seq_item_port.item_done();
-		
-		seq_item_port.get_next_item(tr);
-		driver2dut.reset=tr.rst;
-		seq_item_port.item_done();
-	endtask
-endclass:rst_driver
+    `uvm_component_utils(fifo_driver)
+
+    virtual fifo_intf#(.WIDTH(8)) intf;                                             // Interface for communication with DUT
+    fifo_transaction trans;                                                         // transaction class
+
+    function new(string name = "fifo_driver", uvm_component parent = null);
+        super.new(name, parent);
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+
+        super.build_phase(phase);
+        
+        // Take the interface configuration from the configuration database
+        
+        if(!uvm_config_db#(virtual fifo_intf)::get(this,"","fifo_intf",intf))
+            `uvm_info("FIFO_DRIVER", "uvm_config_db::get failed!", UVM_HIGH)
+    endfunction
+
+    task rst_check();                                                               // Task to handle reset
+        forever begin
+            @(posedge intf.clk);
+            if (intf.rst) begin
+                intf.wr_en = 1'b0;
+                intf.rd_en = 1'b0;
+            end
+        end
+    endtask
+
+    task send_data();                                                               // Task to send data to DUT
+        forever begin                                                               
+            @(posedge intf.clk);                                                    
+            $display("Inside send_data");                                           
+            if (!intf.rst) begin                                                    
+                seq_item_port.get_next_item(trans);                                 
+                if (trans == null)                                                  // Terminate the loop if no more transactions
+                    break;
+                trans.print();
+                #5
+                intf.wr_en   = trans.wr_en;
+                intf.rd_en   = trans.rd_en;
+                intf.data_in = trans.data_in;
+                seq_item_port.item_done();
+            end
+        end
+    endtask
+
+    task run_phase(uvm_phase phase);
+        $display("STarting FIFO_DRIVER run_phase \n");
+        fork
+            rst_check();
+            send_data();
+        join
+    endtask
+
+endclass: fifo_driver
