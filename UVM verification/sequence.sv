06b08bd5da1d179ec920bf0895eaa93e6a9a349f
Vyom <97853828+vyom-elan@users.noreply.github.com>
2024-03-08 23:24:54 +0530

    Update sequence.sv

--- UVM verification/sequence.sv
+++ UVM verification/sequence.sv
@@ -1,79 +1,75 @@
-class FIFO_test_1_sequence extends uvm_sequence#(data_transaction);
-	`uvm_object_utils(FIFO_test_1_sequence)
-	data_transaction tr;
-	function new(string name="data_seq_1");
-		super.new(name);
-	endfunction
-	task body();
-		for(int i=0;i<20;i++)begin
-			tr=data_transaction::type_id::create("tx_data_tr");
-			start_item(tr);
-			if(i<10)
-				begin	
-					if(!tr.randomise()with{tr.put==1'b1;tr.get==1'b0;})
-						begin
-							`uvm_error("Sequence","Randomisation failure for transaction")
-						end
-				end
-			else
-				begin
-					
-					if(!tr.randomise()with{tr.put==1'b0;tr.get==1'b1;})
-						begin
-							`uvm_error("Sequence","Randomisation failure for transaction")
-						end
-				end
-			finish_item(tr);
-		end
-	endtask
-endclass
+// SEQUENCE
+class fifo_test_1_sequence extends uvm_sequence #(fifo_transaction);
 
-class FIFO_test_2_sequence extends uvm_sequence#(data_transaction);
-	`uvm_object_utils(FIFO_test_2_sequence)
-	data_transaction tr;
-	function new(string name="data_seq_2");
-		super.new(name);
-	endfunction
-	task body();
-		for(int i=0;i<40;i++)begin
-			tr=data_transaction::type_id::create("rd_tr");
-			start_item(tr);
-			if(i<4)
-				begin	
-					if(!tr.randomize()with{tr.put==1'b1;tr.get==1'b0;})
-						begin
-							`uvm_error("Sequence","Randomisation failure for transaction")
-						end
-				end
-			else
-				begin
-					
-					if(!tr.randomise()with{tr.put==1'b0;tr.get==1'b1;})
-						begin
-							`uvm_error("Sequence","Randomisation failure for transaction")
-						end
-				end
-			finish_item(tr);
-		end
-	endtask
-endclass
+    `uvm_object_utils(fifo_test_1_sequence)
+    fifo_transaction trans;                                               // Transaction object
 
-class FIFO_rst_sequence extends uvm_sequence#(rst_transaction);
-	`uvm_object_utils(FIFO_rst_sequence)
-	rst_transaction tr;
-	function new(string name="rst_seq");
-		super.new(name);
-	endfunction
-	task body();
-		tr=rst_ransaction::type_id::create("rst_tx_tr");
-		start_item(tr);
-		tr.rst=1'b1;
-		finish_item(tr);
-		
-		tr=rst_ransaction::type_id::create("rst_tx_tr");
-		start_item(tr);
-		tr.rst=1'b0;
-		finish_item(tr);
+    function new(string name = "fifo_seq_1");
+        super.new(name);
+    endfunction
 
-	endtask
-endclass
+    task body();                                                          // Sequence body
+
+        for (int i = 0; i < 40; i++) begin
+
+            $display("Inside fifo_test_1_sequence");
+
+            trans = fifo_transaction::type_id::create("tx_fifo_trans");
+            start_item(trans);
+
+            $display("Inside fifo_test_1_sequence starting item");
+
+            if (i < 20) begin
+                if (!trans.randomize() with {trans.rd_en == 1'b1; trans.wr_en == 1'b0;}) begin
+                    `uvm_error("Sequence", "Randomization failure for trasaction")
+                end
+            end
+
+            else begin
+                if (!trans.randomize() with {trans.rd_en == 1'b0; trans.wr_en == 1'b1;}) begin
+                    `uvm_error("Sequence", "Randomization failure for trasaction")
+                end
+            end
+            finish_item(trans);
+        end
+    endtask
+endclass : fifo_test_1_sequence
+
+
+class fifo_test_2_sequence extends uvm_sequence #(fifo_transaction);
+
+    `uvm_object_utils(fifo_test_2_sequence)
+    fifo_transaction trans;                                             // transaction class
+
+    function new(string name = "fifo_seq_2");
+        super.new(name);
+    endfunction
+
+    task body();                                                        // Sequence body
+
+        for (int i = 0; i < 64; i++) begin
+
+            $display("Inside fifo_test_2_sequence");
+
+            trans = fifo_transaction::type_id::create("tx_fifo_trans");
+            start_item(trans);
+
+            $display("Inside fifo_test_2_sequence starting item");
+
+            if (i < 8) begin
+                if (!trans.randomize() with {trans.rd_en == 1'b1; trans.wr_en == 1'b0;}) begin
+                    `uvm_error("Sequence", "Randomization failure for trasaction")
+                end
+            end
+
+            else begin
+                if (!trans.randomize() with {trans.rd_en == 1'b1; trans.wr_en == 1'b1;}) begin
+                    `uvm_error("Sequence", "Randomization failure for trasaction")
+                end
+            end
+            finish_item(trans);
+        end
+    endtask
+endclass : fifo_test_2_sequence
+
+//------------------------------------------------------------------------------------------
