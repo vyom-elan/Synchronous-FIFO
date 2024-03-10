a4f1c365fd0a934e79ce5cb12f728ab6c93977d7
Vyom <97853828+vyom-elan@users.noreply.github.com>
2024-03-10 23:32:55 +0530

    Update scoreboard.sv

--- UVM verification/scoreboard.sv
+++ UVM verification/scoreboard.sv
@@ -1,36 +1,42 @@
-// fifo_checker scoreboard
+// SCOREBOARD
 class my_scoreboard extends uvm_scoreboard;
-  `uvm_component_utils(my_scoreboard);
-  
-  uvm_blocking_get_port #(data_transaction)exp_port;
-  uvm_blocking_get_port #(data_transaction)act_port;
-  data_transaction tr_act, tr_exp;
-  bit result;
-  
-  function new(string name, uvm_component parent);
-    super.new(name, parent);
-  endfunction
-  
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    exp_port = new("exp_port", this);
-    act_port = new("act_port", this);
-  endfunction
-  
-  task run_phase(uvm_phase phase);
-    forever
-      begin
-        exp_port.get(tr_exp);
-        act_port.get(tr_act);
-        result = tr_exp.compare(tr_act);
-        if(result)
-          $display("Compare SUCCESS");
-        else
-          `uvm_warning("WARNING", "FAILED COMPARE")
-          $display("THE EXPECTED DATA IS");
-        tr_exp.print();
-        $display("ACTUAL DATA");
-        tr_act.print();
-      end
-  endtask
-endclass:my_scoreboard
+	`uvm_component_utils(my_scoreboard);
+
+    uvm_blocking_get_port #(fifo_transaction) exp_port;         // Expected transaction port
+    uvm_blocking_get_port #(fifo_transaction) act_port;	        // Actual transaction port
+	
+    fifo_transaction fifo_act, fifo_exp;                        // Actual and expected transactions
+    
+	bit result;                                                 // Result of comparison
+    	
+	function new(string name, uvm_component parent);
+		super.new(name, parent);
+	endfunction	
+    
+	function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+		exp_port = new("exp_port", this);
+		act_port = new("act_port", this);		
+	endfunction
+    
+	task run_phase(uvm_phase phase);
+		forever begin
+            
+            $display("Inside scoreboard");
+
+			exp_port.get(fifo_exp);
+			act_port.get(fifo_act);
+			result = fifo_exp.compare(fifo_act);
+			if (result)
+				$display("Compare SUCCESSFULLY");
+			else begin
+                `uvm_warning("WARNING", "Compare FAILED");
+            end
+                
+			$display("The expected data is");
+			fifo_exp.print();
+			$display("The actual data is");
+			fifo_act.print();	
+		end
+	endtask		
+endclass : my_scoreboard
