9c470bcd04160a70c25cc428fec12235a4b7e0fa
Vyom <97853828+vyom-elan@users.noreply.github.com>
2024-03-12 23:17:57 +0530

    Update sequencer.sv

--- UVM verification/sequencer.sv
+++ UVM verification/sequencer.sv
@@ -1,14 +1,13 @@
-class data_sequencer extends uvm_sequencer#(data_transaction);
-	`uvm_object_utils(data_sequencer)
-	function new(string name="data_sequencer", uvm_component parent);
-		super.new(name);
-	endfunction
-endclass:data_sequencer
 
+// SEQUENCER
+class fifo_sequencer extends uvm_sequencer#(fifo_transaction);
 
-class rst_sequencer extends uvm_sequencer#(rst_transaction);
-	`uvm_object_utils(rst_sequencer)
-	function new(string name="rst_sequencer", uvm_component parent);
-		super.new(name);
-	endfunction
-endclass:rst_sequencer
+    `uvm_component_utils(fifo_sequencer)
+
+    function new(string name="fifo_sequencer", uvm_component parent);
+        super.new(name);
+    endfunction
+
+endclass:fifo_sequencer
+
+//-----------------------------------------------------------------------------------------------
